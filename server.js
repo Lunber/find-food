08d0a11cda51f810666dae6e40d4b27f403dee85
Lunber <891504539@qq.com>
2016-09-23 11:54:09 +0800

    读写文件

--- server.js
+++ server.js
@@ -9,14 +9,56 @@ var cors = require('koa-cors');
 var co = require('co');
 var serve = require('koa-static')
 var session = require('koa-session');
-// var mongoose = require('mongoose');
+var koaBody = require('koa-body')();
+var fs = require('fs')
 
+var api_router = new Router({
+    prefix: '/api'
+});
 
+var date = new Date();
+var time = {
+    date: date,
+    year: date.getFullYear(),
+    month: date.getFullYear() + "-" + (date.getMonth() + 1),
+    day: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
+    minute: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+    date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes())
+}
+
+api_router.post('/intention',koaBody,function *(next) {
+    console.log(this.request.body)
+    var body = this.request.body
+    var data =
+    // data.push(JSON.stringify(this.request.body))
+    // console.log(data)
+    fs.open('./data/'+time.day+'.json', 'a' , (err , fd) => {
+        fs.readFile('./data/'+time.day+'.json', {encoding: 'utf-8'} ,(err, fileData) => {
+            //文件内容
+            if (fileData == false){
+                data.push(JSON.stringify(this.request.body))
+                console.log('none')
+            }else {
+                data = JSON.parse(fileData)
+                data.push(JSON.stringify(this.request.body))
+            }
+            // var jsonData = JSON.parse(fileData)
+            fs.writeFile(fd,data,(err) => {
+            });
+        })
+    })
+    this.response.body = {
+        msg: 'success',
+        code: '1'
+    }
+})
 
 app.use(json());
 app.use(cors());
 app.keys = ['some secret hurr'];
 app.use(session(app));
+
+app.use(api_router.routes())
 
 app.use(serve('dist'))
 
